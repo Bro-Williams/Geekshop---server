097b8d9e6be0b47937a98b720c4b9f2078b707c4
Bro-Williams <ad-gloriam@mail.ru>
2021-05-20 14:13:05 +0300

    added remove users

--- adminapp/views.py
+++ adminapp/views.py
@@ -41,6 +41,13 @@ def admin_users_update(request, user_id):
     return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
+def admin_users_remove(request, user_id):
+    user = User.objects.get(id=user_id)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+
+
 
 
 
