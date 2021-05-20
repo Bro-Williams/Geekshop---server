e4305e6dc7cfa66216c6546a7e367430e29ff500
Bro-Williams <ad-gloriam@mail.ru>
2021-05-20 13:59:22 +0300

    added update users

--- adminapp/views.py
+++ adminapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
 def index(request):
@@ -14,9 +14,10 @@ def admin_users_read(request):
     return render(request, 'adminapp/admin-users-read.html', context)
 
 
+
 def admin_users_create(request):
     if request.method == 'POST':
-        form = UserAdminRegisterForm(data=request.POST)
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
@@ -24,3 +25,24 @@ def admin_users_create(request):
         form = UserAdminRegisterForm()
     context = {'form': form}
     return render(request, 'adminapp/admin-users-create.html', context)
+
+
+
+def admin_users_update(request, user_id):
+    selected_user = User.objects.get(id=user_id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=selected_user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
+    context = {'form': form, 'selected_user': selected_user}
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
+
+
+
+
+
+
+
